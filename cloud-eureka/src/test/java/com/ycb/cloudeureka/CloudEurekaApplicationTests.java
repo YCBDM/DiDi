b957eb85b180b4be0294c6059dcc9a2bef97ed24
835135815@qq.com <835135815@qq.com>
2021-01-08 17:59:06 +0800

    DiDi项目搭建

---
+++ cloud-eureka/src/test/java/com/ycb/cloudeureka/CloudEurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ycb.cloudeureka;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
